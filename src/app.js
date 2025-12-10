b183708602c776f363d04c52f89cdd3a9c60a3f4
Samara Alcantara <samara31oliveira@gmail.com>
2025-12-10 15:15:41 -0300

    add comentários

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemLivro = require("./models/SchemaLivro.js");
-//const conectarDB = require('./config/db.js');
-//const PORT = 3000;
+const conectarDB = require("./config/db.js");
+const PORT = 3000;
 require("dotenv").config();
-//const {midDiaSemana} = require('./middleware/diaValido.js');
+const { midDiaSemana } = require("./middleware/diaValido.js");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(midDiaSemana);
+app.use(midDiaSemana);
 //conectarDB(); //chama a conexão com o db
 
 app.use("/", require("./routes/routesEstante.js"));
